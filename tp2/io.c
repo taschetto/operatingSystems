03eea01818e45d1690e85bab97d762c0c362f55b
Rodrigo Dias Correa <rdiascorrea@gmail.com>
2014-11-02 20:27:59 -0200

    Basic file (low level) functions implemented.
    - Functions to manipulate the image file
      (create/load/unload/read/write).
    
    - Created a simple main file, just to test the new functions.

--- tp2/io.c
+++ tp2/io.c
@@ -3,5 +3,135 @@
  * @date   02/11/2014
  */
 
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "io.h"
+
+#define NO_DESCRIPTOR -1
+int current_descriptor = NO_DESCRIPTOR;
+
+
+int create_new_fs(const char *fs_image_name)
+{
+	int fd;
+	int status;
+	int image_size = CLUSTER_SIZE * NUMBER_OF_CLUSERS;
+	char *initial_data;
+
+	// create image file
+	fd = open(fs_image_name, O_RDWR|O_CREAT|O_TRUNC, S_IRWXU);
+	if (fd > 0) {
+		initial_data = malloc(image_size);
+		if (initial_data != NULL) {
+			memset(initial_data, 0, image_size);
+			// fill the file with zeros
+			status = write(fd, initial_data, image_size);
+			if (status == image_size) {
+				status = 0;
+			} else {
+				status = -1;
+			}
+			//
+			free(initial_data);
+		} else {
+			status = -1;
+		}
+		//
+		close(fd);
+	} else {
+		printf("couldn't create '%s'\n", fs_image_name);
+		status = -1;
+	}
+
+	return status;
+}
+
+int load_fs(const char *fs_image_name)
+{
+	int fd;
+	int status;
+
+	if (current_descriptor == NO_DESCRIPTOR) {
+		fd = open(fs_image_name, O_RDWR);
+		if (fd > 0) {
+			current_descriptor = fd;
+			status = 0;
+		} else {
+			status = -1;
+		}
+	} else {
+		status = -1;
+	}
+
+	return status;
+}
+
+int unload_fs()
+{
+	int status;
+
+	if (current_descriptor != NO_DESCRIPTOR) {
+		close(current_descriptor);
+		current_descriptor = NO_DESCRIPTOR;
+		status = 0;
+	} else {
+		status = -1;
+	}
+
+	return status;
+
+}
+
+int read_cluster(unsigned int cluster_id, char *buffer)
+{
+	int status;
+
+	if (current_descriptor != NO_DESCRIPTOR) {
+		status = lseek(current_descriptor, cluster_id * CLUSTER_SIZE, SEEK_SET);
+		if (status >= 0) {
+			status = read(current_descriptor, buffer, CLUSTER_SIZE);
+			if (status == CLUSTER_SIZE) {
+				status = 0;
+			} else {
+				status = -1;
+			}
+		} else {
+			status = -1;
+		}
+	} else {
+		status = -1;
+	}
+
+	return status;
+
+}
+
+int write_cluster(unsigned int cluster_id, const char *buffer)
+{
+	int status;
+
+	if (current_descriptor != NO_DESCRIPTOR) {
+		status = lseek(current_descriptor, cluster_id * CLUSTER_SIZE, SEEK_SET);
+		if (status >= 0) {
+			status = write(current_descriptor, buffer, CLUSTER_SIZE);
+			if (status == CLUSTER_SIZE) {
+				status = 0;
+			} else {
+				status = -1;
+			}
+		} else {
+			status = -1;
+		}
+	} else {
+		status = -1;
+	}
+
+	return status;
+}
+
 
 
--- tp2/io.h
+++ tp2/io.h
@@ -8,8 +8,17 @@
 #define IO_H
 
 #define CLUSTER_SIZE 2048
+#define NUMBER_OF_CLUSERS 1024
 
-int read_cluster(int id, char *buffer);
-int write_cluster(int id, const char *buffer);
+// Creates a new zeroed file (not formated!)
+int create_new_fs(const char *fs_image_name);
+// Loads an existing image file
+int load_fs(const char *fs_image_name);
+// Unloads the image currently in use
+int unload_fs();
+// Reads a cluster from the loaded image
+int read_cluster(unsigned int cluster_id, char *buffer);
+// Writes a cluster to the loaded image
+int write_cluster(unsigned int cluster_id, const char *buffer);
 
 #endif // IO_H
